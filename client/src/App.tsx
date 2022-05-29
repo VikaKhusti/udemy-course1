6d111ed15a5dbccad43d1a2a5500a26f73a641b8
vkhusti@eccentex.com <evox11668>
2022-05-29 21:42:55 +0300

    styling, comment blocks, looping elements, using js-code inside of react component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,38 @@
+const productArr = [
+  {
+    id: 1,
+    name: 'item 1',
+    price: 5
+  },
+  {
+    id: 2,
+    name: 'item 2',
+    price: 8
+  },
+  {
+    id: 3,
+    name: 'item 3',
+    price: 11
+  }
+]
+
+
 function App() {
   return (
-    <div>
-      <h1>Re-Store</h1>
+    <div className="app">
+      <h1 style={{color: 'black'}}>Re-Store</h1> {/* inline styling */}
+      {console.log('hello, motherfucker!')}
+      <h6>|-------------------------------|</h6>
+      {productArr.map((item /*, index */) => ( 
+          /* rounded brackets help to avoid return statement, and used for one line of code which is going to be returned
+            while curly brackets require return */
+          <h4 key={item.id}>{item.name} - ${item.price}</h4>  
+          /* a key with its unique value should be added to each element which has been created in loop */
+          /* one of solution whould be using second argument of map - index */
+        ))
+      }
+      <h6>|-------------------------------|</h6>
+      <h3>Total Amount: {productArr.length}</h3>
     </div>
   );
 }
