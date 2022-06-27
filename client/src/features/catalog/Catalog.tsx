2758b8c7ad6cf72f60a15cb95907c77416b72a22
vkhusti@eccentex.com <evox11668>
2022-06-27 21:29:45 +0300

    created separate react-components

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,5 +1,6 @@
-import { Avatar, Button, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import { Button } from "@mui/material";
 import { Product } from "../../app/models/product"
+import ProductList from "./ProductList";
 
 interface Props {
     products: Product[];
@@ -9,23 +10,7 @@ interface Props {
 export default function Catalog({products, addProduct}: Props){
     return (
         <>
-            <List>
-                {products.map((product, index ) => ( 
-                    /* rounded brackets help to avoid return statement, and used for one line of code which is going to be returned
-                    while curly brackets require return */
-                    <ListItem key={index}>
-                        <ListItemAvatar>
-                            <Avatar src={product.pictureUrl}/>
-                        </ListItemAvatar>
-                        <ListItemText>
-                            {product.name} - ${product.price}
-                        </ListItemText>
-                    </ListItem>  
-                    /* a key with its unique value should be added to each element which has been created in loop */
-                    /* one of solution whould be using second argument of map - index */
-                ))
-                }
-            </List>
+            <ProductList products={products} />
 
             <h3>Total Amount: {products.length}</h3>
             <Button variant='contained' onClick={addProduct}>Add a Product</Button>
