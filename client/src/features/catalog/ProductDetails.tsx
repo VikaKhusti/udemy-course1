8a7d56cd742485bc5644d7d18ba5306b4b966ad4
vkhusti@eccentex.com <evox11668>
2022-06-29 18:42:23 +0300

    installed axios for fetching data + add functionality to More-button

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,7 +1,27 @@
 import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import axios from "axios"; 
+import { useParams } from "react-router-dom";
+import { Product } from "../../app/models/product";
 
 export default function ProductDetails() {
+    const {id} = useParams<{id: string}>();
+
+    const [product, setProduct] = useState<Product | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/api/products/${id}`)
+            .then(response => setProduct(response.data))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false));
+    }, [id])
+
+    if(loading) return <Typography variant="h3">Loading...</Typography>
+
+    if(!product) return <Typography variant="h3">Product not Found!</Typography>
+
     return (
-        <Typography variant="h2">Item</Typography>
+        <Typography variant="h2">{product.name}</Typography>
     )
 }
