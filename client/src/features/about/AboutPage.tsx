75313e86cd3331ade67a81792c33dd3f7715232f
vkhusti@eccentex.com <evox11668>
2022-07-10 18:26:49 +0300

    error handling

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,7 +1,17 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Container, Typography } from "@mui/material";
+import agent from "../../app/api/agent";
 
 export default function AboutPage() {
     return (
-        <Typography variant="h2">About us</Typography>
+        <Container>
+            <Typography gutterBottom variant="h2">Test Errors</Typography>
+            <ButtonGroup fullWidth>
+                <Button variant="contained" onClick={() => agent.TestError.get400Error().catch(error => console.log(error))}>Test 400 Error</Button>
+                <Button variant="contained" onClick={() => agent.TestError.get401Error().catch(error => console.log(error))}>Test 401 Error</Button>
+                <Button variant="contained" onClick={() => agent.TestError.get404Error().catch(error => console.log(error))}>Test 404 Error</Button>
+                <Button variant="contained" onClick={() => agent.TestError.get500Error().catch(error => console.log(error))}>Test Server Error</Button>
+                <Button variant="contained" onClick={() => agent.TestError.getValidationError()}>Test Validation Error</Button>
+            </ButtonGroup>
+        </Container>
     )
 }
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import axios from "axios"; 
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
+import agent from "../../app/api/agent";
 
 export default function ProductDetails() {
     const {id} = useParams<{id: string}>();
@@ -11,8 +12,8 @@ export default function ProductDetails() {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get(`http://localhost:5000/api/products/${id}`)
-            .then(response => setProduct(response.data))
+        agent.Catalog.details(parseInt(id))
+            .then(response => setProduct(response))
             .catch(error => console.log(error))
             .finally(() => setLoading(false));
     }, [id])
