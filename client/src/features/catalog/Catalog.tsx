889207d3f819130a2bac2f6906c5d71908fb16b9
vkhusti@eccentex.com <evox11668>
2022-07-24 18:31:20 +0300

    add Loading Component

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,15 +1,22 @@
 import { useState, useEffect } from "react";
 import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product"
 import ProductList from "./ProductList";
 
 export default function Catalog(){
 
     const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        agent.Catalog.list().then(products => setProducts(products))
+        agent.Catalog.list()
+        .then(products => setProducts(products))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false))
     }, []) /* [] means that this code will be run just once, but not every time component load */
+
+    if(loading) return <LoadingComponent message="Loading..."/>;
 
     return (
         <>
